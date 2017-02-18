415060f260d5250f1e2fb4af58d4bd99f8e926c7
Ruslan Babych <ruslanb@amazon.com>
2017-02-18 11:18:35 -0800

    Init

---
+++ Sources/Ferry/Domain/TerminalService.swift
@@ -0,0 +1,58 @@
+//
+//  TerminalService.swift
+//  Kitura-Starter
+//
+//  Created by Babych, Ruslan on 2/16/17.
+//
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+public class TerminalService : WSDOTService {
+    public init() {
+        super.init(key: "2d1dbff1-4218-4e8b-b832-e6599db7919b")
+    }
+}
+
+public class WSDOTService {
+    var key: String = "2d1dbff1-4218-4e8b-b832-e6599db7919b"
+    
+    init(key: String) {
+        self.key = key
+    }
+    
+    public func get(path: String,
+                    method: String,
+                    completionHandler: @escaping (JSON) -> Void)
+    {
+        print("http://www.wsdot.wa.gov/ferries/api/\(path)/rest/\(method)")
+        
+        let parameters: Parameters = ["apiaccesscode": key]
+        Alamofire
+            .request("http://www.wsdot.wa.gov/ferries/api/\(path)/rest/\(method)", parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .downloadProgress(closure: { (Progress) in
+                print(Progress.fractionCompleted)
+            })
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                case .failure(let error):
+                    print(error)
+                }
+                
+                
+                print(response.request!)  // original URL request
+                print(response.response!) // HTTP URL response
+                print(response.data!)     // server data
+                print(response.result)   // result of response serialization
+                    
+                let json = JSON(response.result.value!)
+                completionHandler(json);
+        }
+    }
+}
---
+++ Sources/Server/FerryController.swift
@@ -0,0 +1,30 @@
+////
+////  FerryController.swift
+////  Kitura-Starter
+////
+////  Created by Babych, Ruslan on 2/16/17.
+////
+////
+//
+import Foundation
+import Kitura
+import SwiftyJSON
+import LoggerAPI
+import Ferry
+
+public class FerryController {
+    /**
+     * Handler for getting an application/json response.
+     */
+    public func getTerminal(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        Log.debug("GET - /json route handler...")
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
+        
+        let terminalService = TerminalService()
+        terminalService.get(path: "terminals",
+                            method: "terminalbasics",
+                            completionHandler: { result in
+                                    try! response.status(.OK).send(json: result).end()
+        })
+    }
+}
